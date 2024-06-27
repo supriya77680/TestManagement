1819e932eda35d8f8f02b52578268221e6f02764
Supriya Mahajan <mahajansupriya77@gmail.com>
2024-06-27 11:16:32 +0530

    setting up the project

---
+++ TestManagement/src/main/java/com/bnt/TestManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestManagementApplication.class, args);
+	}
+
+}
